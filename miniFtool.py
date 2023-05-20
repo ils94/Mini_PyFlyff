c0ed19cf2176ea99f94f4e368d3eee9bcb11542b
None <None>
2023-05-20 09:23:00 -0300

    a small tweak to prevent breaking mini ftool

--- miniFtool.py
+++ miniFtool.py
@@ -6,13 +6,18 @@ import globalVariables
 
 def mini_ftool(check):
     while True:
-        if globalVariables.on:
-            for key, timer in zip(globalVariables.mini_ftool_keys, globalVariables.mini_ftool_key_timers):
-                browserControl.firefox_window(key)
+        try:
+            if globalVariables.on:
+                for key, timer in zip(globalVariables.mini_ftool_keys, globalVariables.mini_ftool_key_timers):
+                    browserControl.firefox_window(key)
 
-                if check == 1:
-                    time.sleep(random.uniform(0, float(timer)))
-                else:
-                    time.sleep(float(timer))
+                    if check == 1:
+                        time.sleep(random.uniform(0, float(timer)))
+                    else:
+                        time.sleep(float(timer))
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+            continue
 
         time.sleep(1)
